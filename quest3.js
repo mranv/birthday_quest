acd1a377f8c89a7308d4bd43eaa6314ea5d65735
Tania Klymchuk <tania.klymchuk@basetis.com>
2023-05-07 23:55:12 +0200

    [REF] Corrected quest 3 notions

--- quest3.js
+++ quest3.js
@@ -13,43 +13,25 @@ const stave2 = new VF.Stave(10, 150, 400);
 stave2.addClef("treble");
 stave2.setContext(context).draw();
 
-const stave3 = new VF.Stave(10, 250, 400);
-stave3.addClef("treble");
-stave3.setContext(context).draw();
-
-const stave4 = new VF.Stave(10, 350, 400);
-stave4.addClef("treble");
-stave4.setContext(context).draw();
-
 const chords1 = [
-  new VF.StaveNote({ keys: ["e/4", "g/4", "c/5"], duration: "q" }),
-  new VF.StaveNote({ keys: ["d/4", "g/4", "b/4"], duration: "q" }),
-  new VF.StaveNote({ keys: ["e/4", "a/4", "c/5"], duration: "q" }),
-  new VF.StaveNote({ keys: ["f/4", "a/4", "c/5"], duration: "q" }),
+  new VF.StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "q" }),
+  new VF.StaveNote({ keys: ["c/4", "f/4", "a/4"], duration: "q" }),
+  new VF.StaveNote({ keys: ["b/3", "d/4", "g/4"], duration: "q" }),
+  new VF.StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "q" }),
 ];
 
-const chords3 = [
-  new VF.StaveNote({ keys: ["e/4", "a/4", "c/5"], duration: "q" }),
-  new VF.StaveNote({ keys: ["f/4", "a/4", "c/5"], duration: "q" }),
-  new VF.StaveNote({ keys: ["e/4", "g/4", "c/5"], duration: "q" }),
-  new VF.StaveNote({ keys: ["d/4", "g/4", "b/4"], duration: "q" }),
-];
 
 const voice1 = new VF.Voice({ num_beats: 4, beat_value: 4 });
 voice1.addTickables(chords1);
 
-const voice3 = new VF.Voice({ num_beats: 4, beat_value: 4 });
-voice3.addTickables(chords3);
-
 const formatter = new VF.Formatter()
-  .joinVoices([voice1, voice3])
-  .format([voice1, voice3], 350);
+  .joinVoices([voice1])
+  .format([voice1], 350);
 
 voice1.draw(context, stave1);
-voice3.draw(context, stave3);
 
-const correctAnswers = ["C", "G", "AM", "F", "DM", "EM", "F", "G"];
-const inputIds = ["q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8"];
+const correctAnswers = ["AM", "DM", "E", "AM"];
+const inputIds = ["q1", "q2", "q3", "q4"];
 const submitBtn = document.getElementById("submitBtn");
 const feedback = document.getElementById("feedback");
 
